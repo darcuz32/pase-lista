90818160b08a81be8e6a3191a238a9bd45a35be0
Roberto Garcia <darcuz32@gmail.com>
2022-10-25 22:49:18 -0500

    Ajusta base url

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'http://lespcoah.ddns.net/pase-lista/';
-
 /*
 |--------------------------------------------------------------------------
 | Index File
